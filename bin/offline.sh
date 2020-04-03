c3bf9c9e041236d1f71011f92863fdf5f05e1e50
Juha Alanen <juha.alanen@mapbox.com>
2020-04-03 15:31:32 +0300

    [metrics] Use real world style for TTRC measurement

--- bin/offline.sh
+++ bin/offline.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-OFFLINE=./build/macos/Release/mbgl-offline
+OFFLINE=./build/bin/mbgl-offline
 
 # Barcelona
 $OFFLINE --north 41.4664 --west 2.0407 --south 41.2724 --east 2.2680 --output barcelona.db
